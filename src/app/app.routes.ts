3e9e04e1df82f0e6f8393a30ce5535787471c01e
Sebastian <naesebastian96@gmail.com>
2026-02-26 15:42:36 +0200

    Added dashboard component + routing. Added basic guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,28 @@
 import { Routes } from '@angular/router';
 import { HomeShellComponent } from './lib/home-feat/ui/home-component-shell/home-shell.component';
 import { AuthenticationShellComponent } from './lib/authentication-feat/ui/authentication-shell/authentication-shell.component';
+import { DashboardShellComponent } from './lib/dashboard-feat/ui/dashboard-shell/dashboard-shell.component';
+import { authenticationGuard } from './lib/authentication-feat/data-access/authentication-guard-guard';
 
 export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'home'
-    },
-    {
-        path: 'home',
-        component: HomeShellComponent
+        redirectTo: 'dashboard/home'
     },
     {
         path: 'login',
         component: AuthenticationShellComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardShellComponent,
+        canActivateChild: [authenticationGuard],
+        children: [
+            {
+                path: 'home',
+                component: HomeShellComponent
+            },
+        ]
     }
 ];
---
+++ src/app/lib/dashboard-feat/ui/dashboard-shell/dashboard-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-shell.component',
+  imports: [],
+  templateUrl: './dashboard-shell.component.html',
+  styleUrl: './dashboard-shell.component.scss',
+})
+export class DashboardShellComponent {
+
+}
