b6421e1f16f0f9b5941f3ad39d967c12db3e0106
Sebastian <naesebastian96@gmail.com>
2026-02-26 21:12:38 +0200

    Refined the view for login-box component

--- src/app/lib/authentication-feat/ui/login-box/login-box.component.ts
+++ src/app/lib/authentication-feat/ui/login-box/login-box.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-login-box',
-  imports: [],
+  imports: [MatFormFieldModule, MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './login-box.component.html',
   styleUrl: './login-box.component.scss',
 })
