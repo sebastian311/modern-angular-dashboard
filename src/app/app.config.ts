e0d98dc9e6c440b7b2d0951bf9c7f08d62804d03
Sebastian <naesebastian96@gmail.com>
2026-02-26 16:06:08 +0200

    Added mocked BE for endpoints calling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { mockBackendInterceptor } from './data-access/mock-backend.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([mockBackendInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
---
+++ src/app/data-access/mock-backend.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse, HttpInterceptorFn, HttpResponse } from "@angular/common/http";
+import { of, throwError } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+export const mockBackendInterceptor: HttpInterceptorFn = (req, next) => {
+    const { url, method, body } = req;
+    const networkDelay = 750;
+
+    // LOGIN ENDPOINT
+    if( url.endsWith('/login') && method === 'POST') {
+        const credentials = body as { username?: string; password?: string } | null;
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        if(username === 'admin' && password === 'admin') {
+            return of(new HttpResponse({
+                status: 200,
+                body: {token: 'fake-jwt-token'}
+            })).pipe(delay(networkDelay))
+        }
+
+        return throwError(() => new HttpErrorResponse({
+            status: 401,
+            statusText: 'Unauthorized',
+            error: { message: 'Invalid username or password' }
+        })).pipe(delay(networkDelay));
+    }
+
+    return next(req);
+}
