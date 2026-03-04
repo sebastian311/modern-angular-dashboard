26a3dd4aeb6fbca0fa39ee876522516052ba3ff3
Sebastian <naesebastian96@gmail.com>
2026-03-04 13:02:44 +0200

    Added sidenav shell to dashboard-shell component

--- src/app/lib/dashboard-feat/ui/dashboard-shell/dashboard-shell.component.ts
+++ src/app/lib/dashboard-feat/ui/dashboard-shell/dashboard-shell.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-dashboard-shell.component',
-  imports: [],
+  imports: [MatSidenavModule],
   templateUrl: './dashboard-shell.component.html',
   styleUrl: './dashboard-shell.component.scss',
 })
