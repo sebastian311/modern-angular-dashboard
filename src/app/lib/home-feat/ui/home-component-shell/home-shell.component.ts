d57663ba509070b8a2e72ba3482006e8c5ca9b21
Sebastian <naesebastian96@gmail.com>
2026-03-06 13:11:59 +0200

    Started home-page implementation (WIP)

--- src/app/lib/home-feat/ui/home-component-shell/home-shell.component.ts
+++ src/app/lib/home-feat/ui/home-component-shell/home-shell.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { AuthStore } from '../../../authentication-feat/data-access/authentication.store';
 
 @Component({
   selector: 'app-home-component-shell',
-  imports: [],
+  imports: [RouterModule, MatDividerModule],
   templateUrl: './home-shell.component.html',
   styleUrl: './home-shell.component.scss',
 })
 export class HomeShellComponent {
-
+  private authStore = inject(AuthStore);
+  username = computed(() => 
+    this.authStore.userProfile().username 
+      ? this.authStore.userProfile().username
+      : sessionStorage.getItem("username") 
+  );
 }
