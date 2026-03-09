fc8f74fbbb87df3b46c3f17179b92bdba6b057d9
Sebastian <naesebastian96@gmail.com>
2026-03-09 13:48:34 +0200

    Added basic styling to user-profile-information component

--- src/app/lib/user-profile-feat/ui/user-profile-information/user-profile-information.component.ts
+++ src/app/lib/user-profile-feat/ui/user-profile-information/user-profile-information.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-profile-information.component',
-  imports: [],
+  selector: 'app-user-profile-information',
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
   templateUrl: './user-profile-information.component.html',
   styleUrl: './user-profile-information.component.scss',
 })
 export class UserProfileInformationComponent {
+  editMode = signal<boolean>(false);
 
+  constructor(private router: Router) {}
+
+  goBack(): void {
+    this.router.navigate(['/dashboard']);
+  }
 }
--- src/app/lib/user-profile-feat/ui/user-profile-shell/user-profile-shell.component.ts
+++ src/app/lib/user-profile-feat/ui/user-profile-shell/user-profile-shell.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { UserProfileInformationComponent } from "../user-profile-information/user-profile-information.component";
+import { AuthStore } from '../../../authentication-feat/data-access/authentication.store';
 
 @Component({
   selector: 'app-user-profile-shell.component',
-  imports: [],
+  imports: [ UserProfileInformationComponent ],
   templateUrl: './user-profile-shell.component.html',
   styleUrl: './user-profile-shell.component.scss',
 })
 export class UserProfileShellComponent {
-
+  private authStore = inject(AuthStore);
+  userProfile = this.authStore.userProfile;
+  sessionUsername = signal(sessionStorage.getItem("username") || "");
 }
